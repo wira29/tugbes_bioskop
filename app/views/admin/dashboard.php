c13ad8fb6f6045b412a59a9a1f32674cc6d0abab
AdamPoi <isadamrr@gmail.com>
2022-12-21 07:02:10 +0700

    feat: Add UserController feat: Add AdminUserController feat(models): Add User Model feat(views): Add dashboard & user feat(views): Moving login & register to views

---
+++ app/views/admin/dashboard.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Admin</title>
+
+  <link rel="stylesheet" href="../assets/css/style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+
+  <script src="https://kit.fontawesome.com/75fbb137eb.js" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+  <div class="contianer-fluid override">
+    <div class="row flex-nowrap mw-100">
+      <aside id="sidebar" class="col-auto collapse collapse-horizontal show min-vh-100 text-white bg-theme-tertiary " id="sidebar">
+        <div class="d-flex flex-column p-3">
+          <h2 class="d-flex align-items-center me-md-auto mb-3">Admin</h2>
+          <ul class="nav nav-pills gap-3 flex-column flex-shrink-0 mb-auto">
+            <li class="nav-item"><a class="nav-link link-active" href="">Menu 1</a></li>
+            <li class="nav-item"><a class="nav-link text-white bg-theme-tertiary" href="">Menu2asadaa</a></li>
+            <li class="nav-item"><a class="nav-link text-white bg-theme-tertiary" href="">Menu3</a></li>
+            <li class="nav-item"><a class="nav-link text-white bg-theme-tertiary" href="">Menu4</a></li>
+            <li class="nav-item"><a class="nav-link text-white bg-theme-tertiary" href="">Menu5</a></li>
+          </ul>
+        </div>
+      </aside>
+      <main class="col pt-4">
+        <header class="container d-flex align-items-center gap-3">
+          <a class="text-theme-primary d-md-none" data-bs-toggle="collapse" href="#sidebar" role="button"><i class="fa-solid fa-bars fa-xl mb-3"></i></a>
+          <h2>Dashboard z</h2>
+        </header>
+        <article class="container">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum minima laborum reprehenderit porro ea. Repellat veniam voluptatum ex consequatur expedita culpa, porro, ullam aperiam omnis nam harum, ipsum sapiente odit.
+        </article>
+      </main>
+    </div>
+  </div>
+
+</body>
+
+</html>
