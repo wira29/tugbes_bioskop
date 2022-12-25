71ee5fd338644a1a8aae75af80503e40bf885e84
Wira29 <bag4sk4r4@gmail.com>
2022-12-25 18:11:54 +0700

    add checkout

--- app/controllers/CheckoutController.php
+++ app/controllers/CheckoutController.php
@@ -1,18 +1,30 @@
 <?php 
 
 require_once __DIR__ . '/../models/FilmModel.php';
+require_once __DIR__ . '/../models/CheckoutModel.php';
 
 class CheckoutController extends Controller
 {
     private $filmModel;
+    private $model;
 
     public function __construct()
     {
         $this->filmModel = new FilmModel();
+        $this->model = new CheckoutModel();
     }
     
     public function index(int $idFilm)
     {
+        $d = $this->model->getJadwalByFilm($idFilm, '2022-12-26');
+        // $teater = array_merge_recursive($d[0], $d[1]);
+        // print_r($teater);
+        $teater = [1];
+        foreach($d as $d){
+            $teater[$d['id_teater']] = [$teater[$d['id_teater']], $d['waktu']];
+        }
+        print_r($teater);
+        die();
         $data = [
             'film'  => $this->filmModel->getById($idFilm),
         ];
--- app/models/CheckoutModel.php
+++ app/models/CheckoutModel.php
@@ -8,4 +8,12 @@ class CheckoutModel
   {
     $this->db = Koneksi::getInstance()->getPDO();
   }
+
+  public function getJadwalByFilm(int $idFilm, string $tanggal)
+  {
+    $sql = "SELECT * FROM jadwal where id_film = '$idFilm' and tanggal = '$tanggal'";
+    $result = $this->db->query($sql);
+
+    return $result->fetchAll();
+  }
 }
