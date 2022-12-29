ba060d0dc54ab3d0cd1e63b7ad67de414e6d4780
percyfikri <mafikri27@gmail.com>
2022-12-29 20:13:31 +0700

    fix(views:home.php): desc&cover film

--- app/views/dashboard/home.php
+++ app/views/dashboard/home.php
@@ -8,24 +8,24 @@
     <div class="carousel-item active">
       <div class="p-5 mb-4 rounded-3 jumbotron" style="background-image: url('<?= $uriHelper->baseUrl('assets/img/jumbotron.png') ?>');">
       <div class="container py-5">
-        <h1 class="fw-bold">Get Tiket</h1>
-        <p class="col-md-6 fs-5">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
+        <h1 class="fw-bold">Black Adam</h1>
+        <p class="col-md-6 fs-5">Berkisah tentang sosok antihero yang mendapatkan kekuatan dari dewa mesir bernama Adam. Ia datang untuk menciptakan keadilan di dunia saat ini.</p>
       </div>
     </div>
     </div>
     <div class="carousel-item">
-      <div class="p-5 mb-4 rounded-3 jumbotron" style="background-image: url('<?= $uriHelper->baseUrl('assets/img/jumbotron.png') ?>');">
+      <div class="p-5 mb-4 rounded-3 jumbotron" style="background-image: url('<?= $uriHelper->baseUrl('assets/img/film/cover/peaky-blinders.png') ?>');">
       <div class="container py-5">
-        <h1 class="fw-bold">Get ABCDEF</h1>
-        <p class="col-md-6 fs-5">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
+        <h1 class="fw-bold">Peaky Blinders</h1>
+        <p class="col-md-6 fs-5">Peaky Blinders adalah serial televisi drama kriminal Britania Raya yang dibuat oleh Steven Knight. Berlatar di Birmingham, Inggris, serial ini mengikuti eksploitasi keluarga mafia Shelby setelah berlangsungnya Perang Dunia I.</p>
       </div>
     </div>
     </div>
     <div class="carousel-item">
-      <div class="p-5 mb-4 rounded-3 jumbotron" style="background-image: url('<?= $uriHelper->baseUrl('assets/img/jumbotron.png') ?>');">
+      <div class="p-5 mb-4 rounded-3 jumbotron" style="background-image: url('<?= $uriHelper->baseUrl('assets/img/film/cover/mortal-kombat.png') ?>');">
       <div class="container py-5">
-        <h1 class="fw-bold">Get GHIJ</h1>
-        <p class="col-md-6 fs-5">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
+        <h1 class="fw-bold">Mortal Kombat</h1>
+        <p class="col-md-6 fs-5">Mortal Kombat adalah serial film aksi seni bela diri Amerika yang didasarkan pada serial video game pertarungan dengan nama yang sama oleh Midway Games. Film pertama diproduksi oleh Threshold Entertainment milik Lawrence Kasanoff.</p>
       </div>
     </div>
     </div>
